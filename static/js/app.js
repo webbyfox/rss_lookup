6a1f631ce037bdff0d9dc3ccd27c2c3ea188d781
Rizwan Mansuri <mr.mansuri@gmail.com>
2017-01-21 00:18:52 +0000

    updated api url for domain indenpencies

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ var myApp = angular.module('MyApp', []);
 
 myApp.controller("MyController",function($scope, $http)
  {
-   $http.get('http://webbyfox:5000/api/rss').success(function(response){
+   $http.get('/api/rss').success(function(response){
 
    $scope.articles = response.Response.rss.channel.item;
    $scope.orderPub='pubDate';
