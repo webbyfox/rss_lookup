2b42e43bd0e405623b5f188a8b5cf25cd2563a8f
Rizwan Mansuri <mr.mansuri@gmail.com>
2017-01-22 07:48:25 +0000

    added ng reference

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import abort
-from flask import  jsonify
+from flask import jsonify
 from flask import request
 
 import urllib2
